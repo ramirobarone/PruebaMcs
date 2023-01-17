9d3e6c65e69d09ce257871796eb87c37caf097f1
rbarone <rbarone@PXSNTAR-16>
2023-01-17 09:39:22 -0300

    agregar keyvault

--- PruebasMcsGithub/Program.cs
+++ PruebasMcsGithub/Program.cs
@@ -1,3 +1,4 @@
+using Azure.Identity;
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,6 +7,17 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+string clientId = "25aa5cfe-8ce5-425a-92fa-1780e3a411fc";
+string clientSecret = "Lwc8Q~59UuQTm.3kjhJCDaLgm~MNieWDY4ujtc0W";
+string tenantId = "84d11978-3c07-4fcb-867d-6a2e0b94887f";
+
+var keyVaultName = "masassessmentvault";
+
+ClientSecretCredential clientSecretCredential = new(tenantId, clientId, clientSecret);
+builder.Configuration.AddAzureKeyVault(new System.Uri($"https://{keyVaultName}.vault.azure.net/"), clientSecretCredential);
+
+var keyVaultSection = builder.Configuration.GetSection("JwtMas");
 
 var app = builder.Build();
 
