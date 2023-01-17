cdd280a256f985130dfc164a73e669130dc14655
rbarone <rbarone@PXSNTAR-16>
2023-01-17 09:44:09 -0300

    prueba vault

--- PruebasMcsGithub/Controllers/WeatherForecastController.cs
+++ PruebasMcsGithub/Controllers/WeatherForecastController.cs
@@ -1,4 +1,7 @@
+using Azure.Identity;
+using Azure.Security.KeyVault.Secrets;
 using Microsoft.AspNetCore.Mvc;
+using System.Runtime.CompilerServices;
 
 namespace PruebasMcsGithub.Controllers
 {
@@ -30,9 +33,38 @@ namespace PruebasMcsGithub.Controllers
             .ToArray();
         }
         [HttpGet(Name = "OtroEndpoint")]
-        public string otroPrueba()
+        public async Task<IActionResult> otroPrueba()
         {
-            return "Hola";
+            const string secretName = "JwtMas";
+            var keyVaultName = "MasAssessments";
+            var kvUri = $"https://{keyVaultName}.vault.azure.net";
+            var client = new SecretClient(new Uri(kvUri), new DefaultAzureCredential());
+
+            var secretValue = "";
+
+            //Console.Write($"Creating a secret in {keyVaultName} called '{secretName}' with the value '{secretValue}' ...");
+            //await client.SetSecretAsync(secretName, secretValue);
+            //Console.WriteLine(" done.");
+
+            //Console.WriteLine("Forgetting your secret.");
+            //secretValue = string.Empty;
+            //Console.WriteLine($"Your secret is '{secretValue}'.");
+
+            Console.WriteLine($"Retrieving your secret from {keyVaultName}.");
+            var secret = await client.GetSecretAsync(secretName);
+            Console.WriteLine($"Your secret is '{secret.Value.Value}'.");
+
+            //Console.Write($"Deleting your secret from {keyVaultName} ...");
+            //DeleteSecretOperation operation = await client.StartDeleteSecretAsync(secretName);
+            //// You only need to wait for completion if you want to purge or recover the secret.
+            //await operation.WaitForCompletionAsync();
+            //Console.WriteLine(" done.");
+
+            //Console.Write($"Purging your secret from {keyVaultName} ...");
+            //await client.PurgeDeletedSecretAsync(secretName);
+            //Console.WriteLine(" done.");
+
+            return Ok(secret);
         }
     }
 }
