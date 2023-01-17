709968abb6f409e690a5a4d056da13113a42ebf1
rbarone <rbarone@PXSNTAR-16>
2023-01-17 10:18:20 -0300

    hgola

--- PruebasMcsGithub/Controllers/WeatherForecastController.cs
+++ PruebasMcsGithub/Controllers/WeatherForecastController.cs
@@ -38,12 +38,12 @@ namespace PruebasMcsGithub.Controllers
             try
             {
 
-                const string secretName = "JwtMas";
-                var keyVaultName = "masassessmentvault";
-                var kvUri = $"https://{keyVaultName}.vault.azure.net";
-                var client = new SecretClient(new Uri(kvUri), new DefaultAzureCredential());
+                //const string secretName = "JwtMas";
+                //var keyVaultName = "masassessmentvault";
+                //var kvUri = $"https://{keyVaultName}.vault.azure.net";
+                //var client = new SecretClient(new Uri(kvUri), new DefaultAzureCredential());
 
-                var secretValue = "";
+                //var secretValue = "";
 
                 //Console.Write($"Creating a secret in {keyVaultName} called '{secretName}' with the value '{secretValue}' ...");
                 //await client.SetSecretAsync(secretName, secretValue);
@@ -53,9 +53,9 @@ namespace PruebasMcsGithub.Controllers
                 //secretValue = string.Empty;
                 //Console.WriteLine($"Your secret is '{secretValue}'.");
 
-                Console.WriteLine($"Retrieving your secret from {keyVaultName}.");
-                var secret = await client.GetSecretAsync(secretName);
-                Console.WriteLine($"Your secret is '{secret.Value.Value}'.");
+                //Console.WriteLine($"Retrieving your secret from {keyVaultName}.");
+                //var secret = await client.GetSecretAsync(secretName);
+                //Console.WriteLine($"Your secret is '{secret.Value.Value}'.");
 
                 //Console.Write($"Deleting your secret from {keyVaultName} ...");
                 //DeleteSecretOperation operation = await client.StartDeleteSecretAsync(secretName);
@@ -66,7 +66,7 @@ namespace PruebasMcsGithub.Controllers
                 //Console.Write($"Purging your secret from {keyVaultName} ...");
                 //await client.PurgeDeletedSecretAsync(secretName);
                 //Console.WriteLine(" done.");
-                return Ok(secret);
+                return Ok("Hola");
             }
             catch (Exception ex)
             {
